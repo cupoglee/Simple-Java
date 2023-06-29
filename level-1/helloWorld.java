598c093dd5dafb2cf82886393daf50b2c03a7bec
Smruthi <138140448+cupoglee@users.noreply.github.com>
2023-06-29 21:24:15 +0530

    first github commit

---
+++ level-1/helloWorld.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class helloWorld {
+    public static void main(String x[]) {
+        int meonly, hehe, Iamgrt;
+        Scanner im = new Scanner(System.in);
+
+        System.out.println("Enter some 2 shits.");
+        meonly = im.nextInt();
+        hehe = im.nextInt();
+        Iamgrt = meonly - hehe;
+        System.out.println("ans yours =" + Iamgrt);
+
+    }
+
+}
