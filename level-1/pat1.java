2ebd9358fb1da2922e0a0e52ba2829788ba9b30b
Smruthi <138140448+cupoglee@users.noreply.github.com>
2023-07-01 12:57:03 +0530

    Q : Printing a pattern - ###

---
+++ level-1/pat1.java
@@ -0,0 +1,14 @@
+import java.io.*; //### 
+                  //## 
+                  //#
+
+public class pat1 {
+    public static void main(String args[]) {
+        for (int i = 3; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
